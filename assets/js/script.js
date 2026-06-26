e92906f6489e2b07d8ee63f64ba991cb17580b9b
nirmal <nirmalpatidar6688@gmail.com>
2026-06-26 11:56:30 +0530

    1st commit

---
+++ assets/js/script.js
@@ -0,0 +1,115 @@
+'use strict';
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const searchInput = document.querySelector(".input-field");
+  const items = document.querySelectorAll(".decoration"); // 🔥 CHANGE
+
+  searchInput.addEventListener("input", function () {
+
+    let value = searchInput.value.toLowerCase();
+
+    items.forEach(function (li) {
+
+      let title = li.querySelector(".card-title");
+
+      if (!title) return;
+
+      let text = title.textContent.toLowerCase();
+
+      if (text.includes(value)) {
+        li.style.display = "";   // show
+      } else {
+        li.style.display = "none"; // hide
+      }
+
+    });
+
+  });
+
+});
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn active when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const showElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", showElemOnScroll);
+
+
+
+/**
+ * product filter
+ */
+
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterBox = document.querySelector("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  filterBox.setAttribute("data-filter", this.dataset.filterBtn)
+}
+
+addEventOnElem(filterBtns, "click", filter);
